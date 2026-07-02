b2731f32dd25d575004fc165981959f24c14ad7d
DaniRubertone <rubertone.daniel.l@gmail.com>
2026-07-02 14:57:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,31 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+const CACHE_VERSION = 'v3';
+const CACHE_NAME = `jmm-${CACHE_VERSION}`;
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(['./index.html', './manifest.json', './LogotipoLMM.jpeg'])
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(event.request, copy));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+  }
+});
